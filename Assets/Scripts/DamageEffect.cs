7c821f0bf7480fcdfedd49512c8848d265dbb3f9
cacaocoffee <75840431+cacaocoffee@users.noreply.github.com>
2021-01-04 21:10:09 +0900

    데미지창
    투명해지는 효과 구현완료

--- Assets/Scripts/DamageEffect.cs
+++ Assets/Scripts/DamageEffect.cs
@@ -9,22 +9,22 @@ public class DamageEffect : MonoBehaviour
 
     public float MoveSpeed; //텍스트 이동속도
     public float disSpeed; //투명해지는 속도
-    public float destroyTime;
-    private TextMeshPro Damagetext; // 데미지 플로팅 텍스트
-    private Color alpha; //원래 텍스트 색
+    public float destroyTime; //사라지는 시간
+    public TextMeshProUGUI Damagetext; // 데미지 플로팅 텍스트
+    public Color a; //원래 텍스트 색 
  
     private void Start()
     {
-        Damagetext = GetComponent<TextMeshPro>();
-        //alpha = Damagetext.color;
+        Damagetext = GetComponent<TextMeshProUGUI>();
+        a = Damagetext.color;
         Invoke("DestroyObject", destroyTime);
     }
 
     private void Update()
     {
         transform.Translate(new Vector3(0, MoveSpeed * Time.deltaTime, 0));//
-        //alpha.a = Mathf.Lerp(alpha.a, 0f,Time.deltaTime * disSpeed); // 텍스트 투명해지기
-        //Damagetext.color = alpha;
+        a.a = Mathf.Lerp(a.a, 0f,Time.deltaTime * disSpeed); // 텍스트 투명해지기
+        Damagetext.color = a;
     }
 
     public void DestroyObject()
