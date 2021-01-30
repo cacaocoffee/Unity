24cf8bd150af5cdefacd22839897906e3b12bdf0
cacaocoffee <75840431+cacaocoffee@users.noreply.github.com>
2021-01-31 00:04:11 +0900

    스킬매니저초반
    스킬매니저 만드는중

---
+++ Assets/Scripts/UI/SkilManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkilManager : Singleton<UImanager> 
+{
+
+    public int[,] SkillList = new int[5,3];
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
